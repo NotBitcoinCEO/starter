d1148c10799e96a745b65f3d3a9e8563815516ca
seanryanbyu <linklayton@gmail.com>
2023-11-02 16:43:54 -0700

    setup basic website structure

--- server.js
+++ server.js
@@ -6,15 +6,33 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+/* ******************************************
+ * Default GET route
+ * ***************************************** */
+app.get("/", (req, res) => {res.send("Welcome home!")})
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-
+app.use(require("./routes/static"))
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
